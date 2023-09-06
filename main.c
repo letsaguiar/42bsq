da60e39b0d7e0103a726d439bce9ecaa9fec3952
lde-agui <lde-agui@student.42.fr>
2023-09-06 10:14:26 -0300

    add support to multiple file input

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lde-agui <lde-agui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 18:46:04 by leticia-agu       #+#    #+#             */
-/*   Updated: 2023/09/06 09:55:04 by lde-agui         ###   ########.fr       */
+/*   Updated: 2023/09/06 10:13:53 by lde-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,35 @@
 #include "ft_map/ft_map.h"
 #include "ft_string/ft_string.h"
 
+void	process_maps(char **maps)
+{
+	int				i;
+	char			**map;
+	t_map_config	*config;
+
+	i = 1;
+	while (maps[i])
+	{
+		config = load_map_config(maps[i]);
+		if (!validate_map(maps[i], config))
+		{
+			ft_putstr("map error");
+		}
+		else
+		{
+			map = load_map(maps[i], config);
+			print_biggest_square(map, config);
+			destroy_map(map);
+		}
+		ft_putstr("\n");
+		free(config);
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	t_map_config	*config;
-	char			**map;
-
 	if (argc > 1)
-	{
-		config = load_map_config(argv[1]);
-		if (!validate_map(argv[1], config))
-		{
-			ft_putstr("map error\n");
-			return (1);
-		}
-		map = load_map(argv[1], config);
-		print_biggest_square(map, config);
-		destroy_map(map);
-		free(config);
-	}
+		process_maps(argv);
 	return (0);
 }
